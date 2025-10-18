8372fd427926090e019fa2dfc8b3db7058d87c9c
bongs20 <2005syaiful@gmail.com>
2025-10-19 07:11:13 +0800

    allowd host

--- redditblog/settings.py
+++ redditblog/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-redditblog-secret-key-for-local'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["blogbebas.onrender.com"]
 
 # Application definition
 INSTALLED_APPS = [
